2b83022a57f097990b8c9cf41449751a67af5da1
Luca Macesanu <53663808+SkipperOnEdge@users.noreply.github.com>
2020-03-27 16:48:10 -0500

    Initial Documents

---
+++ javascript (1).js
@@ -0,0 +1,64 @@
+var albums = [];
+  
+
+    function createAlbum(){
+
+        var a = document.getElementById("album").value;
+        var art = document.getElementById("artist").value;
+        var gen = document.getElementById("genre").value;
+        var r = document.getElementById("release").value;
+
+      var add = {
+        "album": a,
+        "artist": art,
+        "genre": gen,
+        "release": r
+      }
+
+      
+      albums.push(add);
+      console.log(albums);
+      localStorage.setItem("myAlbums",JSON.stringify(albums));
+    }
+
+    function del() {
+      albums = JSON.parse(localStorage.getItem("myAlbums"));
+      albu = document.getElementById("a");
+      albumName = albu.options[albu.selectedIndex];
+      albums.splice(albu.selectedIndex-1,1);
+      localStorage.setItem("myAlbums", JSON.stringify(albums));
+      console.log(albums);
+      console.log(albumName);
+    }
+
+    function up() {
+      albums = JSON.parse(localStorage.getItem("myAlbums"));
+      albu = document.getElementById("a");
+      albumName = albu.options[albu.selectedIndex];
+      albums.splice(albu.selectedIndex-1,1);
+
+      var a = document.getElementById("album").value;
+        var art = document.getElementById("artist").value;
+        var gen = document.getElementById("genre").value;
+        var r = document.getElementById("release").value;
+
+      var add = {
+        "album": a,
+        "artist": art,
+        "genre": gen,
+        "release": r
+      }
+
+      
+      albums.push(add);
+      console.log(albums);
+      localStorage.setItem("myAlbums",JSON.stringify(albums));
+    }
+
+    function ret() {
+      albums = JSON.parse(localStorage.getItem("myAlbums"));
+      albu = document.getElementById("a");
+      albumName = albu.options[albu.selectedIndex];
+
+      document.write(JSON.stringify(albums[albu.selectedIndex-1],null,2));
+    }
