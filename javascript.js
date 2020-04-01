bec414df0c25c267ea7c3e48613d32954db6609f
Luca Macesanu <53663808+SkipperOnEdge@users.noreply.github.com>
2020-03-31 19:55:09 -0500

    fixDisplay

--- javascript.js
+++ javascript.js
@@ -15,9 +15,9 @@ var albums = [];
         "team": a,
         "wins": art,
         "losses": gen,
-        "offense rating": r,
-        "defense rating": e,
-        "shooting percentage": d
+        "offenseRating": r,
+        "defenseRating": e,
+        "shootingPercentage": d
       }
 
 
@@ -43,11 +43,11 @@ var albums = [];
       albums.splice(albu.selectedIndex-1,1);
 
       var a = document.getElementById("team").value;
-        var art = document.getElementById("win").value;
-        var gen = document.getElementById("loss").value;
-        var r = document.getElementById("OR").value;
-        var e = document.getElementById("DR").value;
-        var d = document.getElementById("shoot").value;
+      var art = document.getElementById("win").value;
+      var gen = document.getElementById("loss").value;
+      var r = document.getElementById("OR").value;
+      var e = document.getElementById("DR").value;
+      var d = document.getElementById("shoot").value;
 
       var add = {
         "team": a,
@@ -74,34 +74,34 @@ var albums = [];
       }
     }
 
-    function getDataArray()
-    {
-      albums = JSON.parse(localStorage.getItem("myAlbums"));
-      data = Array(2);
-      for(var r = 0; r < albums.length; r++){
-        for(var c = 0; c < albums[r].length; c++){
-          if(c==0) {
-            data.push(albums[r].getElementById("team"));
-            document.write(albums[r].getElementById("team"));
-          }
-          if(c==1) {
-            data.push(albums[r].getElementById("win"));
-          }
-          if(c==2) {
-            data.push(albums[r].getElementById("loss"));
-          }
-          if(c==3) {
-            data.push(albums[r].getElementById("OR"));
-          }
-          if(c==4) {
-            data.push(albums[r].getElementById("DR"));
-          }
-          if(c==5) {
-            data.push(albums[r].getElementById("shoot"));
-          }
-        }
-      }
-      document.write(JSON.stringify())
-      document.write(data.toString());
+    // function getDataArray()
+    // {
+    //   albums = JSON.parse(localStorage.getItem("myAlbums"));
+    //   data = Array(2);
+    //   for(var r = 0; r < albums.length; r++){
+    //     for(var c = 0; c < albums[r].length; c++){
+    //       if(c==0) {
+    //         data.push(albums[r].getElementById("team"));
+    //         document.write(albums[r].getElementById("team"));
+    //       }
+    //       if(c==1) {
+    //         data.push(albums[r].getElementById("win"));
+    //       }
+    //       if(c==2) {
+    //         data.push(albums[r].getElementById("loss"));
+    //       }
+    //       if(c==3) {
+    //         data.push(albums[r].getElementById("OR"));
+    //       }
+    //       if(c==4) {
+    //         data.push(albums[r].getElementById("DR"));
+    //       }
+    //       if(c==5) {
+    //         data.push(albums[r].getElementById("shoot"));
+    //       }
+    //     }
+    //   }
+    //   document.write(JSON.stringify())
+    //   document.write(data.toString());
 
-    }
+    // }
